ae7c7c99aca8da06e973d05186c28ddac67b50a7
Pablo Ramos <pablo-362@hotmail.com>
2021-12-27 20:36:38 -0300

    first part working, api google sheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import {AppProps} from 'next/app'
+import React from "react";
+import { ChakraProvider } from '@chakra-ui/react';
+import {AppProps} from "next/app";
 
-const App: React.FC<AppProps> = ({ Component, pageProps })=>  {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import {GetStaticProps} from 'next'
-import {Product} from '../product/types'
-import api from '../product/api';
-
+import React from "react";
+import {GetStaticProps} from "next";
+import {Product} from "../product/types";
+import api from "../product/api";
 interface Props {
   products: Product[];
 }
-
-const IndexRoute: React.FC<Props> = ({products})=>{
-  return <div>{JSON.stringify(products)}</div>;
-}
-
-export const getStaticProps: GetStaticProps = async ()=> {
+const IndexRoute: React.FC<Props> = ()=>{
+  return <div>{`<IndexRoute />`}</div>;
+};
+export const getStaticProps: GetStaticProps = async () => {
   const products = await api.list();
   return {
     props: {
       products,
     },
   };
-}
-
+};
 export default IndexRoute;
